6622d9b3e72f32f01cfe56420dad26e011555deb
modert <modert@gmail.com>
2023-10-18 15:04:20 -0600

    added modal javascript

--- js/creative.js
+++ js/creative.js
@@ -46,3 +46,15 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
